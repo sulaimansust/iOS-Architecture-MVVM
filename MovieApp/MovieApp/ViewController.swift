19eb009b76c2593729a3f81cdc59228222678fc8
Sulayman Md <sulayman.md@stud.uni-bamberg.de>
2022-12-22 19:10:57 +0100

    Create initial project

---
+++ MovieApp/MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Md Sulayman on 22.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
