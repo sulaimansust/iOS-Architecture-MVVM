a22dbb455859203e3062540c873f60cefb2e8487
Sulayman Md <sulaiman.sust@gmail.com>
2022-12-26 03:52:38 +0100

    Add unit test for MovieInfo

---
+++ MovieApp/MovieAppTests/MovieInfoTests.swift
@@ -0,0 +1,52 @@
+//
+//  MovieInfoTests.swift
+//  MovieAppTests
+//
+//  Created by Md Sulayman on 26.12.22.
+//
+
+import XCTest
+
+final class MovieInfoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+    
+    func testCreateInstance() throws{
+        let movieInfo = MovieInfo(colorIndex: 1, imdbId: "tt0800369")
+        XCTAssertNotNil(movieInfo)
+    }
+    
+    func testProvideReturnUnmodifiedData() throws {
+        /// Arrange
+        let movieInfo = MovieInfo(colorIndex: 1, imdbId: "tt0800369")
+
+        XCTAssertEqual(movieInfo.colorIndex, 1)
+        XCTAssertEqual(movieInfo.imdbId, "tt0800369")
+        
+    }
+    
+   
+
+}
